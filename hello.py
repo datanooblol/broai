535bb9dc3571ab4d434939716c53f7d1a540977f
EC2 Default User <ec2-user@ip-172-16-48-255.ap-southeast-1.compute.internal>
2025-04-16 02:32:18 +0000

    init new broai

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from broai!")
-
-
-if __name__ == "__main__":
-    main()
--- tests/test_yo_bro.py
+++
@@ -1,5 +0,0 @@
-from broai.greeting import yo_bro
-
-
-def test_yo_bro():
-    assert yo_bro() == {"response": "Yo Bro!"}
